999a37af29fb1c74b2fc2018e817fbc954e46fed
prabha442 <93595774+prabha442@users.noreply.github.com>
2022-12-21 13:37:46 +0530

    Update provider.tf

--- ecs/provider.tf
+++ ecs/provider.tf
@@ -1,8 +1,9 @@
 provider "aws" {
-  access_key = "add-your-key"
-  secret_key = "add-your-access_key"
+ # access_key = "add-your-key"
+ # secret_key = "add-your-access_key"
+  
+ #if you are running from AWS ec2 linux instance please use bellow credentials section
   region     = var.aws_region
-  #if you are running from AWS ec2 linux instance please use bellow credentials section
-  #shared_credentials_file = "$HOME/.aws/credentials"
-  #profile = "default"
+  shared_credentials_file = "$HOME/.aws/credentials"
+  profile = "testecs"
 }
