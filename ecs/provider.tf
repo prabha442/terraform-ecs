e38ca31c492787d4b9fc7c24dd4437512a0003b3
prabha442 <93595774+prabha442@users.noreply.github.com>
2023-01-03 20:24:12 +0530

    Update provider.tf

--- ecs/provider.tf
+++ ecs/provider.tf
@@ -1,6 +1,4 @@
 provider "aws" {
- # access_key = "add-your-key"
- # secret_key = "add-your-access_key"
   
  #if you are running from AWS linux instance please use bellow credentials section
   region     = var.aws_region
